a6781ca31dadb15668392c21e8ec317edeb1e084
Luis Masuelli <luis@datagran.io>
2023-02-10 14:41:11 -0500

    For version 0.0.2, using the new types

--- Runtime/Types/Models/RefMapItemPair.cs
+++ Runtime/Types/Models/RefMapItemPair.cs
@@ -12,7 +12,7 @@ namespace GameMeanMachine.Unity.NetRose.RefMapChars
             {
                 // This is just a pair of elements meant to wrap a RefMap item (with
                 // color variations).
-                public RefMapItem.ColorCode ColorCode;
+                public RefMapAddOn.ColorCode ColorCode;
                 public ushort ItemIndex;
             
                 public void Serialize(Serializer serializer)
@@ -21,14 +21,14 @@ namespace GameMeanMachine.Unity.NetRose.RefMapChars
                     serializer.Serialize(ref ItemIndex);
                 }
 
-                public static explicit operator ItemPair(Tuple<ushort, RefMapItem.ColorCode> value)
+                public static explicit operator ItemPair(Tuple<ushort, RefMapAddOn.ColorCode> value)
                 {
                     return value == null ? null : new ItemPair { ColorCode = value.Item2, ItemIndex = value.Item1 };
                 }
             
-                public static implicit operator Tuple<ushort, RefMapItem.ColorCode>(ItemPair pair)
+                public static implicit operator Tuple<ushort, RefMapAddOn.ColorCode>(ItemPair pair)
                 {
-                    return pair == null ? null : new Tuple<ushort, RefMapItem.ColorCode>(pair.ItemIndex, pair.ColorCode);
+                    return pair == null ? null : new Tuple<ushort, RefMapAddOn.ColorCode>(pair.ItemIndex, pair.ColorCode);
                 }
             }
         }
